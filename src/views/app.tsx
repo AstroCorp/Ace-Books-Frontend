6b52ed28babcf75b8e97f6158ef789043c3ecc4d
Alejandro Leal <aleaberm12@gmail.com>
2021-03-13 19:04:24 +0100

    homepage

--- src/views/app.tsx
+++ src/views/app.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from './home';
 import NoMatch from './no_match';
 
 const App = () => (
-	<BrowserRouter basename="/Ace-Books-Frontend">
-		<Switch>
-			<Route exact path="/">
-				<Home />
-			</Route>
-			<Route path="*">
-				<NoMatch />
-			</Route>
-		</Switch>
-	</BrowserRouter>
+	<Switch>
+		<Route exact path="/">
+			<Home />
+		</Route>
+		<Route path="*">
+			<NoMatch />
+		</Route>
+	</Switch>
 );
 
 export default App;
