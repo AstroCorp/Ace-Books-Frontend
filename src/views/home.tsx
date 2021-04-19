7061c5fbbf6b41530f3a95e056baafb83a01b8d8
Alejandro Leal <aleaberm12@gmail.com>
2021-04-19 21:05:25 +0200

    Diseño home

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -11,35 +11,49 @@ const Home = (props: any) => {
 	}
 
 	return (
-		<div className="bg-index h-screen p-4 flex flex-col">
-			<header>
-				<nav className="flex items-center p-3 flex-wrap">
-				    <Link to="/" className="p-2 mr-4 inline-flex items-center text-white font-sans text-4xl cursor-pointer">
+		<div className="bg-index h-screen flex flex-col">
+			<header className={classNames("md:bg-opacity-0 md:h-auto", {
+				"absolute bg-white bg-opacity-90 h-full w-full": navActive,
+			})}>
+				<nav className="flex items-center p-4 flex-wrap">
+				    <Link to="/" className={classNames("p-2 mr-4 inline-flex items-center text-white md:text-white font-sans text-4xl cursor-pointer", {
+						"text-gray-700 hover:text-darkblue-500": navActive,
+					})}>
 				        Ace Books
 				    </Link>
 
-				    <button className="inline-flex md:hidden ml-auto cursor-pointer" onClick={ toggleNav }>
+				    <button className={classNames("text-white inline-flex md:hidden ml-auto cursor-pointer", {
+						"text-gray-700 hover:text-darkblue-500": navActive,
+					})} onClick={ toggleNav }>
 						<Menu
 							width="32px"
 							height="32px"
+							color="currentColor"
 						/>
 					</button>
 
 				    <div className={classNames("relative w-full md:inline-flex md:flex-grow md:w-auto", {
 						"hidden": !navActive,
-						"block justify-end bg-white bg-opacity-25 py-4 pr-2": navActive,
 					})}>
-				        <ul className="md:inline-flex md:flex-row md:ml-auto md:w-auto w-full md:items-center items-start flex flex-col md:h-auto">
-				            <li className="md:inline-flex md:w-auto w-full items-center justify-center text-right">
-				                <Link className="text-white p-2 hover:underline" to="/">Inicio</Link>
+				        <ul className={classNames("md:inline-flex md:flex-row md:ml-auto md:w-auto w-full md:items-center items-start flex flex-col md:h-auto md:text-right md:mt-0", {
+							"text-center mt-8": navActive,
+						})}>
+				            <li className="md:inline-flex md:w-auto w-full items-center justify-center">
+				                <Link className={classNames("md:text-white p-2 hover:underline", {
+									"text-gray-700 hover:text-darkblue-500": navActive,
+								})} to="/">Home</Link>
 				            </li>
-				            <li className={classNames("md:inline-flex md:w-auto w-full items-center justify-center text-right md:mx-6", {
-								"mt-5 mb-8 md:my-0": navActive
+				            <li className={classNames("md:inline-flex md:w-auto w-full items-center justify-center md:mx-6", {
+								"mt-5 mb-8 md:my-0 text-gray-700 hover:text-darkblue-500": navActive
 							})}>
-				                <Link className="text-white p-2 hover:underline" to="/faqs">Preguntas frecuentes</Link>
+				                <Link className={classNames("md:text-white p-2 hover:underline", {
+									"text-gray-700 hover:text-darkblue-500": navActive,
+								})} to="/faqs">FAQs</Link>
 				            </li>
-				            <li className="md:inline-flex md:w-auto w-full items-center justify-center text-right">
-				                <Link className="text-white p-2 border-2" to="/login">Iniciar Sesión</Link>
+				            <li className="md:inline-flex md:w-auto w-full items-center justify-center">
+				                <Link className={classNames("md:text-white md:border-white p-2 border-2", {
+									"text-gray-700 border-gray-700": navActive,
+								})} to="/login">Sign in</Link>
 				            </li>
 				        </ul>
 				    </div>
@@ -47,20 +61,19 @@ const Home = (props: any) => {
 			</header>
 
 			<div className="h-screen flex flex-col justify-center">
-				<article className="text-white w-full md:w-2/3 md:mx-auto xl:w-1/2 xl:ml-32">
-					<h1 className="text-4xl font-semibold">Sincroniza y disfruta</h1>
+				<article className="text-white w-full p-4 mx-auto md:w-2/3 xl:w-1/2 xl:ml-32">
+					<h1 className="text-4xl font-semibold">Your books always with you.</h1>
 					<p className="text-sm mt-2 mb-8">
-						Ten todos tus libros siempre a mano, retoma tu lectura en cualquier dispositivo, 
-						ya sea en un ordenador, una tablet o un smartphone, crea anotaciones y organiza tu
-						biblioteca como desees.</p>
+						Resume your reading from any device.
+					</p>
 
-					<Link className="text-white p-2 border-2 uppercase" to="/register">Registrarse</Link>
+					<Link className="text-white p-2 border-2 uppercase" to="/register">Sign up</Link>
 				</article>
 			</div>
 
-			<header className="text-white text-center">
+			<footer className="text-white text-center p-4">
 				<p>Copyright &copy; { new Date().getFullYear() } Ace Books. All rights reserved.</p>
-			</header>
+			</footer>
 		</div>
 	);
 }
