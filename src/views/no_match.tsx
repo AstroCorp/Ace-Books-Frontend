83dbcf58326e23db455e2e69e66e85c6bda45cfd
Alejandro Leal <aleaberm12@gmail.com>
2021-05-25 21:51:34 +0200

    Mejora 404

--- src/views/no_match.tsx
+++ src/views/no_match.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
 
-const NoMatch = (props: RouteComponentProps) => {
+const NoMatch = () => {
     const [ t ] = useTranslation('no_match');
 
     return (
-        <div className="bg-books h-screen flex flex-col justify-center">
-            <main className="flex flex-col items-center">
-                <h1 className="text-9xl text-center text-white text-opacity-60">404</h1>
-                <h2 className="text-2xl text-center text-white text-opacity-75 mb-10">{ t('message') }</h2>
+        <div className="bg-books h-screen flex flex-col">
+            <header>
+                <nav className="flex items-center p-4 flex-wrap">
+                    <Link to="/" className="p-2 mr-4 inline-flex items-center text-white md:text-white text-4xl cursor-pointer">
+                        Ace Books
+                    </Link>
+                </nav>
+            </header>
 
-                <Link className="text-white px-4 py-3 border-2 rounded-md uppercase bg-white bg-opacity-0 hover:bg-opacity-20 transition duration-300 ease-in-out" to="/">{ t('button') }</Link>
-            </main>
+            <div className="h-screen flex flex-col justify-center">
+                <article className="flex flex-col p-4 items-center text-center">
+                    <h1 className="text-9xl text-white text-opacity-60">404</h1>
+                    <h2 className="text-2xl text-white text-opacity-75 mb-10">{ t('message') }</h2>
+
+                    <Link className="text-white px-4 py-3 border-2 rounded-md uppercase bg-white bg-opacity-0 hover:bg-opacity-20 transition duration-300 ease-in-out" to="/">{ t('button') }</Link>
+                </article>
+            </div>
+
+            <footer className="text-white text-shadow text-center p-4">
+                <p>Copyright &copy; { new Date().getFullYear() } Ace Books. { t('footer') }</p>
+            </footer>
         </div>
     );
 }
