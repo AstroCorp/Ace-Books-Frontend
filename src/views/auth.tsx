1aa95575d43112f9d6a54c43a2ebd959e6860af5
Alejandro Leal <aleaberm12@gmail.com>
2021-05-28 00:25:01 +0200

    Mejora auth

--- src/views/auth.tsx
+++ src/views/auth.tsx
@@ -3,6 +3,7 @@ import classNames from "classnames";
 import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
 import { AuthRouteParams } from '../types';
+import { CloseIcon } from '../images/icons';
 
 const Auth = (props: RouteComponentProps<AuthRouteParams>) => {
     const [ option, setOption ] = useState<string>(props.match.params.option || 'login');
@@ -13,17 +14,18 @@ const Auth = (props: RouteComponentProps<AuthRouteParams>) => {
 
     return (
         <div className="min-h-screen bg-books flex flex-col justify-center">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md m-6">
+            <div className="m-16 sm:m-6 sm:mx-auto sm:w-full sm:max-w-md">
                 <Link to="/" className="block mb-6 text-shadow text-center text-4xl cursor-pointer text-white md:text-white">
 				    Ace Books
 				</Link>
 
-                {
-                    option !== 'recovery' && (
-                        <>
+                <div className="flex flex-row relative">
+                    {
+                        option !== 'recovery' && (
+                            <>
                             <button
                                 onClick={() => setOption('login')}
-                                className={classNames("w-1/2 text-gray-700 bg-white text-xl font-semibold px-6 py-4 rounded-tl-md focus:outline-none", {
+                                className={classNames("z-10 w-1/2 text-gray-700 bg-white text-xl font-semibold px-6 py-4 rounded-tl-md focus:outline-none", {
                                     "shadow-inner-full bg-opacity-85": option !== 'login',
                                 })}>
                                 Log in
@@ -31,16 +33,37 @@ const Auth = (props: RouteComponentProps<AuthRouteParams>) => {
                             
                             <button
                                 onClick={() => setOption('register')}
-                                className={classNames("w-1/2 text-gray-700 bg-white text-xl font-semibold px-6 py-4 rounded-tr-md focus:outline-none", {
+                                className={classNames("z-10 w-1/2 text-gray-700 bg-white text-xl font-semibold px-6 py-4 rounded-tr-md focus:outline-none", {
                                     "shadow-inner-full bg-opacity-85": option !== 'register',
                                 })}>
                                 Sign up
                             </button>
-                        </>
-                    )
-                }
-                
-                <div className={classNames("px-6 py-8 sm:px-6 lg:px-8 bg-white rounded-b-md", {
+                            </>
+                        )
+                    }
+
+                    <div className="w-full flex justify-end ml-14 absolute z-0">
+                        {
+                            option === 'recovery' ? (
+                                <button 
+                                    onClick={() => setOption('login')}
+                                    className="p-2 rounded-md hover:bg-white hover:bg-opacity-20 transition duration-300 ease-in-out cursor-pointer"
+                                >
+                                    <CloseIcon height="32px" width="32px" color="#FFF" />
+                                </button>
+                            ) : (
+                                <Link 
+                                    to="/"
+                                    className="p-2 rounded-md hover:bg-white hover:bg-opacity-20 transition duration-300 ease-in-out cursor-pointer"
+                                >
+                                    <CloseIcon height="32px" width="32px" color="#FFF" />
+                                </Link>
+                            )
+                        }
+                    </div>
+                </div>
+                    
+                <div className={classNames("px-6 py-8 lg:px-8 bg-white rounded-b-md", {
                     'rounded-t-md': option === 'recovery',
                 })}>
                     {
@@ -53,7 +76,7 @@ const Auth = (props: RouteComponentProps<AuthRouteParams>) => {
                     <div className="mt-1">
                         <input type="email" className="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded-md" />
                     </div>
-
+                    
                     {
                         option !== 'recovery' && (
                             <>
@@ -64,7 +87,7 @@ const Auth = (props: RouteComponentProps<AuthRouteParams>) => {
                             </>
                         )
                     }
-
+                    
                     {
                         option === 'register' && (
                             <>
@@ -76,7 +99,7 @@ const Auth = (props: RouteComponentProps<AuthRouteParams>) => {
                                 <button type="button" className="w-full mt-4 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                                     Sign up
                                 </button>
-
+                                
                                 <p className="mt-2 text-xs text-center">
                                     By clicking Sign up, you are indicating that you have read and acknowledge the 
                                     <Link target="_blank" className="mx-1 text-blue-500 hover:underline" to="/legal/terms-of-service">Terms of Service</Link>and 
@@ -89,7 +112,7 @@ const Auth = (props: RouteComponentProps<AuthRouteParams>) => {
                     {
                         option === 'login' && (
                             <>
-                                <div className="flex items-center justify-between mt-4">
+                                <div className="flex flex-col sm:flex-row sm:items-center justify-between mt-4">
                                     <div className="flex items-center">
                                         <input id="remember_me" type="checkbox" className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
                                         <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
@@ -97,13 +120,13 @@ const Auth = (props: RouteComponentProps<AuthRouteParams>) => {
                                         </label>
                                     </div>
 
-                                    <div className="text-sm">
+                                    <div className="text-sm mt-2 sm:mt-0">
                                         <button onClick={() => setOption('recovery')} className="font-medium text-blue-600 hover:text-blue-500">
                                             Forgot your password?
                                         </button>
                                     </div>
                                 </div>
-
+                                
                                 <button type="button" className="w-full mt-4 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                                     Log in
                                 </button>
