7d1c1b6aeab62481f0ae884df7e09a63a3a56ca6
Alejandro Leal <aleaberm12@gmail.com>
2021-08-26 00:55:46 +0200

    Mejora en legal y política de cookies

--- src/views/legal.tsx
+++ src/views/legal.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { LegalRouteParams } from '../types';
 import { FaqsIcon, PrivacyIcon, TermsIcon, CookiesIcon } from '../images/icons';
 import { Footer, OfflineHeader } from '../components';
@@ -44,13 +45,13 @@ const Legal = (props: RouteComponentProps<LegalRouteParams>) => {
 	];
 
     return (
-        <div className="bg-books min-h-screen">
+        <div className="bg-books min-h-screen flex flex-col justify-between">
 			<OfflineHeader />
 
-			<div className="flex flex-col justify-center m-6">
+			<div className="flex-1 flex-col justify-center mt-6 mx-6">
 				<div className="w-full lg:w-3/4 xl:w-2/3 p-4 mx-auto flex flex-col md:flex-row">
-					<div className="flex flex-col items-end w-full md:w-96 mr-4">
-				    	<ul className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 gap-4 mb-4 md:mb-0">
+					<div className="w-full md:w-96 mr-4">
+				    	<ul className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 gap-4 mb-4 md:mb-0 sticky top-5">
 							{
 								legalOptions.map((legalOption, index) => (
 									<Link key={'legalOption' + index} className="bg-white bg-opacity-80 rounded-md p-4 md:mb-1 flex flex-row" to={legalOption.url}>
@@ -67,26 +68,29 @@ const Legal = (props: RouteComponentProps<LegalRouteParams>) => {
 				    	</ul>
 					</div>
 					<div className="bg-white bg-opacity-95 p-6 w-full rounded-md">
-						{
-							content.map((text: string, index: number) => (
-								<ReactMarkdown 
-									key={option + '-line-' + index}
-									components={{
-										h1: ({ children }) => (<h1 className="mb-4 text-3xl font-semibold">{ children }</h1>),
-										h2: ({ children }) => (<h2 className="mt-6 text-xl font-bold">{ children }</h2>),
-										h3: ({ children }) => (<h3 className="text-base font-bold">{ children }</h3>),
-										p: ({ children }) => (<p className="py-2">{ children }</p>),
-									}}
-								>
-									{ text }
-								</ReactMarkdown>
-							))
-						}
+						<ReactMarkdown
+							remarkPlugins={[
+								remarkGfm,
+							]}
+							components={{
+								h1: ({ children }) => <h1 className="mb-4 text-3xl font-semibold">{ children }</h1>,
+								h2: ({ children }) => <h2 className="mt-6 text-xl font-bold">{ children }</h2>,
+								h3: ({ children }) => <h3 className="text-base font-bold">{ children }</h3>,
+								p: ({ children }) => <p className="py-2">{ children }</p>,
+								table: ({ children }) => <table className="w-full table-fixed rounded-md overflow-hidden text-left mt-3">{ children }</table>,
+								thead: ({ children }) => <thead className="bg-gray-300 bg-opacity-80 overflow-hidden">{ children }</thead>,
+								tbody: ({ children }) => <tbody className="bg-gray-200 overflow-hidden">{ children }</tbody>,
+								th: ({ children }) => <th className="py-2 px-3">{ children }</th>,
+								td: ({ children }) => <td className="py-2 px-3 border-t border-gray-300">{ children }</td>,
+							}}
+						>
+							{ content.join('\n') }
+						</ReactMarkdown>
 					</div>
 				</div>
 			</div>
 
-			<Footer />
+			<Footer className="p-4" />
 		</div>
     );
 }
