0fdfe711778efc766e60a1450169b2e0f3aaa8e4
Alejandro Leal <aleaberm12@gmail.com>
2021-08-29 23:41:12 +0200

    requestClient

--- src/utils/requestClient.ts
+++ src/utils/requestClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const requestClient = axios.create({
-	baseURL: 'https://ace-books-backend.herokuapp.com/',
+	baseURL: 'https://ace-books-backend.herokuapp.com/api/',
 	responseType: 'json',
     withCredentials: true,
 });
