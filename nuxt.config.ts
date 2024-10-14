7ff7c90847b47a530bfb3a2bc5496514d63ca5b1
Alejandro Leal Bermúdez <aleaberm12@gmail.com>
2024-10-14 23:14:53 +0200

    fix(): Images + .env (#301)
    * temp workflow, image domains
    
    * Add Nitro preset for Vercel deployment
    
    * doc
    
    * fix .env
    
    * fix vercel pull
    
    * fix deploy
    
    * favicon dev mode
    
    * format config
    
    * change favicon dev color
    
    * remove temp workflow, change favicon dev color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,17 +17,28 @@ export default defineNuxtConfig({
 	},
 	app: {
 		head: {
-			link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+			link: [
+				{
+					rel: 'icon',
+					type: 'image/png',
+					href: process.env.NUXT_ENV === "development" ? '/favicon_dev.png' : '/favicon.png',
+				},
+			],
 		},
 	},
+	// Github Actions no reconoce correctamente que se hará un deploy a Vercel
+	// por lo que se debe especificar el preset de Vercel
+	nitro: {
+		preset: 'vercel',
+	},
 	runtimeConfig: {
-		// Private keys are only available on the server
+		// Private keys, se exponen en el servidor
 		nuxtEnv: process.env.NUXT_ENV,
 		nuxtSessionName: process.env.NUXT_SESSION_NAME,
 		nuxtSessionPassword: process.env.NUXT_SESSION_PASSWORD,
 		nuxtSessionTime: process.env.NUXT_SESSION_TIME,
 
-		// Public keys that are exposed to the client
+		// Public keys, se exponen en el cliente
 		public: {
 			backendUrl: process.env.BACKEND_URL,
 			frontendUrl: process.env.FRONTEND_URL,
@@ -80,7 +91,9 @@ export default defineNuxtConfig({
 			alwaysRedirect: true,
 		},
 	},
+	// Para servir nosotros las imágenes y no depender de una API de vercel
 	image: {
 		provider: 'ipx',
+		domains: [process.env.FRONTEND_URL || 'http://localhost:3000'],
 	},
 });
