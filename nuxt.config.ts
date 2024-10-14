ee7f2836ff6300777bcbfcbd20086367bca61a52
Alejandro Leal <aleaberm12@gmail.com>
2024-10-14 23:05:45 +0200

    format config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,13 @@ export default defineNuxtConfig({
 	},
 	app: {
 		head: {
-			link: [{ rel: 'icon', type: 'image/png', href: process.env.NUXT_ENV === "development" ? '/favicon_dev.png' : '/favicon.png' }],
+			link: [
+				{
+					rel: 'icon',
+					type: 'image/png',
+					href: process.env.NUXT_ENV === "development" ? '/favicon_dev.png' : '/favicon.png',
+				},
+			],
 		},
 	},
 	// Github Actions no reconoce correctamente que se hará un deploy a Vercel
